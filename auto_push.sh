6ff8b2e7cd271f7168ac0c828cdd3dd46139a8dc
niu <niu@localhost.localdomain>
2017-06-14 23:03:23 +0800

    commit message

--- auto_push.sh
+++ auto_push.sh
@@ -1,4 +1,26 @@
-git pull origin master
+# update GitHub
+cp ./auto_push.sh ./GitHub/
+cp ./initialize.sh ./GitHub/
+cd /home/niu/backup/GitHub/
 git add --all
 git commit -m "commit message"
 git push -u origin master
+
+# update emacs
+cp -rf /home/niu/.emacs* /home/niu/backup/emacs/
+cd /home/niu/backup/emacs/
+git add --all
+git commit -m "commit message"
+git push -u origin master
+
+# update emacs
+cp -rf /home/niu/.vim* /home/niu/backup/vim/
+cd /home/niu/backup/vim/
+git add --all
+git commit -m "commit message"
+git push -u origin master
+
+
+
+
+#git pull origin master
